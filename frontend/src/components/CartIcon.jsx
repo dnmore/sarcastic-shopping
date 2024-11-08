8ad1d0939c633715946012ef6432ef23763efd8a
Diana Moretti <morettidn@gmail.com>
2024-11-08 14:03:26 +0100

    update carticon

--- frontend/src/components/CartIcon.jsx
+++ frontend/src/components/CartIcon.jsx
@@ -1,47 +1,43 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { CartContext } from "../contexts/CartContext";
 
 import Badge from "@mui/joy/Badge";
 import { IoCart } from "react-icons/io5";
 import { Tooltip } from "@mui/material";
-import Zoom from '@mui/material/Zoom';
-
-
-
-
-
+import Zoom from "@mui/material/Zoom";
 
 const CartIcon = () => {
   const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
+  const [tooltipOpen, setTooltipOpen] = useState(false);
 
-  const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
-
+  const toggleIsCartOpen = () => {
+    setIsCartOpen(!isCartOpen);
+    setTooltipOpen(!tooltipOpen);
+  };
   let cartComment;
 
-  if(cartCount === 0){
-    cartComment = "Your future purchases are waiting... don’t disappoint them!"
-  } else if(cartCount === 1){
-    cartComment = "Look at you, pretending you’re just browsing!"
+  if (cartCount === 0) {
+    cartComment = "Your future purchases are waiting... don’t disappoint them!";
+  } else if (cartCount === 1) {
+    cartComment = "Look at you, pretending you’re just browsing!";
+  } else {
+    cartComment = "This cart’s getting heavy. Ready to lighten your wallet?";
   }
-  else {
-    cartComment = "This cart’s getting heavy. Ready to lighten your wallet?"
-  }
-
 
   return (
     <div className="cursor-pointer" onClick={toggleIsCartOpen}>
-
-      <Tooltip TransitionComponent={Zoom} title={cartComment}>
-<Badge badgeContent={cartCount}>
-      <IoCart className="text-2xl hover:opacity-75" />
-  </Badge>
+      <Tooltip
+        TransitionComponent={Zoom}
+        placement="left"
+        open={tooltipOpen}
+        title={cartComment}
+      >
+        <Badge badgeContent={cartCount}>
+          <IoCart className="text-2xl" />
+        </Badge>
       </Tooltip>
-      
     </div>
-   
   );
 };
 
 export default CartIcon;
-
-
